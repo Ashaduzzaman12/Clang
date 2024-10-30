a9c3cfde84fc823979984aa482a5843367cb4353
S M ASHADUZZAMAN <137910960+Ashaduzzaman12@users.noreply.github.com>
2024-10-30 23:23:21 +0600

    Update YetnotherrokenKeoard.cpp

--- Phitron/day2/YetnotherrokenKeoard.cpp
+++ Phitron/day2/YetnotherrokenKeoard.cpp
@@ -1,29 +1,43 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
-int main(){
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+
+int main()
+{
     int t;
-    cin>>t;
+    cin >> t;
     while (t--)
     {
         string x;
-        cin>>x;
-        stack<char>stu,stl;
-        for(char c:x){
-            if(c!='b'||c!='B'){
-                if(c>='a'&&c<=b){
-                    stl.push(c);
-                }
-                if(c>='A'&&c<='Z'){
-                    stu.push(c);
-                }
+        cin >> x;
+        string y;
+        reverse(x.begin(),x.end());
+        int l = 0, u = 0;
+        for (int i = 0; i < x.size(); i++)
+        {
+            if (x[i] == 'b')
+            {
+                l++;
             }
-            else if(c=='b'){
-                
+            if (x[i] == 'B')
+            {
+                u++;
+            }
+            if (x[i] !='B' && x[i] != 'b')
+            {
+                if (l > 0 && islower(x[i]))
+                {
+                    l--;
+                    continue;
+                }
+                if (u > 0 && isupper(x[i]))
+                {
+                    u--;
+                    continue;
+                }
+                y.push_back(x[i]);
             }
         }
+        reverse(y.begin(), y.end());
+        cout << y << endl;
     }
-    
-    return 0;
 }
