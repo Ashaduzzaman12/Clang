7547494074d96cbaa61b213c0269266834bd80b2
S M ASHADUZZAMAN <137910960+Ashaduzzaman12@users.noreply.github.com>
2024-11-03 22:54:59 +0600

    Update Chemistry.cpp

--- Phitron/week-02/Chemistry.cpp
+++ Phitron/week-02/Chemistry.cpp
@@ -1,1 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve(){
+    string x;
+    int n,m;
+    cin>>n>>m;
+    cin>>x;
+    map<char,int>mx;
+    for(int l:x){
+        mx[l]++;
+    }
+    int cnt=0;
+    for(auto l:mx){
+        if(l.second%2==1) cnt++;
+    }
+    if(cnt<=m+1){
+        cout<<"YES"<<endl;
+    }
+    else{
+        cout<<"NO"<<endl;
+    }
+}
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        solve();
+    }
+    
+    return 0;
+}
 
