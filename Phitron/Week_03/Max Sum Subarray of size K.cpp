f216a59fe34e1b49894a53d1e5c8aa15b1a222d7
S M ASHADUZZAMAN <137910960+Ashaduzzaman12@users.noreply.github.com>
2024-11-24 20:07:27 +0600

    Update Max Sum Subarray of size K.cpp

--- Phitron/Week_03/Max Sum Subarray of size K.cpp
+++ Phitron/Week_03/Max Sum Subarray of size K.cpp
@@ -1,13 +1,17 @@
-int n=a.size(),l=0,r=0;
-    ll cnt=0,sum=0;
+//Max Sum Subarray of size K
+public:
+    int maximumSumSubarray(vector<int>& arr, int k) {
+       int n=arr.size(),l=0,r=0;
+    long long cnt=0,sum=0;
     while (r<n)
     {
-        sum+=a[r];
+        sum+=arr[r];
         if(r-l+1==k){
             cnt=max(cnt,sum);
-            sum-=a[l];
+            sum-=arr[l];
             l++;
         }
         r++;
     }
     return cnt;
+    }
